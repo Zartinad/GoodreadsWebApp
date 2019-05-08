9275c8a275771f7deab38752aed2b281b15b0bbf
Adam <a.zartin@mail.utoronto.ca>
2019-05-08 14:24:40 -0400

    initialized server:

---
+++ server/src/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+
+let port = 8081
+app.listen(process.env.PORT || port)
+console.log("Active on Port: " + port)
+console.log("Revived")
